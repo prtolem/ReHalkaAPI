b04a8f00eed8c1b8995e65cd3c749e70986784f0
Prtolem <66413287+prtolem@users.noreply.github.com>
2022-07-06 21:18:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
-requirements = ["requests"]
-
 setup(
     name='ReHalkaApi',
     version='1.0',
-    packages=[''],
+    packages=['requests'],
     url='https://t.me/derkown',
     license='',
     author='kiber',
     author_email='coder@derkown.ru',
     description='API for https://rehalka.online/',
-    install_requires=requirements
+    url = 'github package source url',
+    download_url = 'download link you saved'
 )
